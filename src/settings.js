d15e9a2c5da32a45c0391cb812a26377edeaa30d
dmc <1755875003@qq.com>
2023-04-12 23:56:00 +0800

    修改为个人项目

--- src/settings.js
+++ src/settings.js
@@ -2,7 +2,7 @@ module.exports = {
   /**
    * @description 网站标题
    */
-  title: 'ELADMIN',
+  title: 'STREAM',
   /**
    * @description 是否显示 tagsView
    */
@@ -38,9 +38,9 @@ module.exports = {
   /**
    * 底部文字，支持html语法
    */
-  footerTxt: '© 2018-2022 Zheng Jie <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License 2.0</a>',
+  footerTxt: '© 2018-2022 Deng Minchuan <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License 2.0</a>',
   /**
    * 备案号
    */
-  caseNumber: ''
+  caseNumber: '蜀ICP备2023004556号-1'
 }
